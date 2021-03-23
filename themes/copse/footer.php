7b46bc00965ce1e7e3fe445173bdce2555124493
Varun Malhotra <varun.malhotra@dcmail.ca>
2021-03-23 12:45:53 -0400

    posts updated

--- themes/copse/footer.php
+++ themes/copse/footer.php
@@ -10,7 +10,40 @@
  */
 
 ?>
+<?php
+$args = array(  
+    'post_type' => 'copse_recipe',
+    'posts_per_page' => 3, 
+);
 
+?>
+
+<div class="large-12 custom_posts"> <h1>Posts</h1> </div>
+
+<?php
+$loop = new WP_Query( $args );
+
+
+while ( $loop->have_posts() ) : $loop->the_post(); ?>
+<div class="cell large-4 box">
+    
+    <h4><?php print the_title(); ?></h4>
+	
+        <?php the_post_thumbnail('medium'); ?>
+    <
+        <?php the_excerpt(); ?> 
+    <?php }  ?>
+
+
+    <a href="<?php the_permalink(); ?>">Read More</a>
+
+</div>
+<?php
+endwhile;
+
+wp_reset_postdata(); 
+
+?>
 	<footer id="colophon" class="site-footer">
 
 	<p>
